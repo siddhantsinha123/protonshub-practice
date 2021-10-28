9bcf654cb7bcdfc271fe1fbe90266258386d7352
siddhantsinha123 <sinhas570@gmail.com>
2021-10-28 13:22:54 +0530

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- root "users#index"
+ root "admin/admin_users#index"
  resources :users do 
    resources :comments
  end 
